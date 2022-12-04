dfa2a8f9337003853a8c81bdd68f8aeac4db1f51
Lewis Nyman <hello@lewisnyman.co.uk>
2022-12-04 14:40:31 +0000

    Getting there

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,15 @@
 module.exports = function(eleventyConfig) {
-  
     eleventyConfig.addPassthroughCopy("assets");
+    eleventyConfig.addPassthroughCopy("json");
+    eleventyConfig.addPassthroughCopy("./*.png");
+    eleventyConfig.addPassthroughCopy("./*.svg");
+    eleventyConfig.addPassthroughCopy("./*.ico");
+
+    eleventyConfig.addPairedShortcode("blogSection", function(content, title) {
+return `<div class="margin-spacing-a">
+<h2 class="color-green-900">${title}</h2>
+<hr class="yellow-500-hr" />
+${content}
+</div>`;
+     });
   };
